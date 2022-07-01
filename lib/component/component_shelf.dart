e3855b88d99007a19276a909003160101f51f62d
VeyselSahn <62219632+Veysel6614@users.noreply.github.com>
2022-07-01 22:50:21 +0300

    app arch prepared & packages installed

---
+++ lib/component/component_shelf.dart
@@ -0,0 +1,2 @@
+export 'constants/constants_shelf.dart';
+export 'widgets/widgets_shelf.dart';
---
+++ lib/core/core_shelf.dart
@@ -0,0 +1,4 @@
+export 'helper/helper_shelf.dart';
+export 'init/init_shelf.dart';
+export 'provider/providers_shelf.dart';
+export 'service/service_shelf.dart';
